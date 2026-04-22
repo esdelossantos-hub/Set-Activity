c776320d7e4fe96609dd69e41a4303446540c062
esdelossantos-hub <esdelossantos@paterostechnologicalcollege.edu.ph>
2026-04-22 15:00:04 +0800

    Create Set Activity_gr5_Delos Santos.py

---
+++ Set Activity_gr5_Delos Santos.py
@@ -0,0 +1,26 @@
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+unique_numbers = set(numbers)
+print(unique_numbers)
+
+fruits = {"apple", "banana"}
+
+fruits.add("orange")
+fruits.remove("banana")
+
+print(fruits)
+
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+print(A | B)
+
+print(A & B)
+
+
+classA = {"John", "Ana", "Mark"}
+classB = {"Ana", "Mark", "Liza"}
+
+print(classA & classB)
+
+print(classA | classB)
